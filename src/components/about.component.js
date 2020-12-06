c48c40c83a6c474814c11fe3af8b336e5af916f9
Isaac <isaaclim95@gmail.com>
2020-12-06 21:16:01 +1000

    worked on it

--- src/components/about.component.js
+++ src/components/about.component.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
+import image1 from '../img/image1.jpg';
 
 export default class About extends Component {
 
   render() {
     return (
       <div className="about" id="about">
-          <h2>Welcome to Maureen's Family Day Care</h2>
+          <img src={image1}></img>
+          <div className="aboutText">
             <p>
                 My name is Maureen and I have been running a family day care at my place for 15 years.
                 I have Certificate III in Childcare and Diploma in Children Services, and a current CPR, Asthma and
@@ -16,6 +18,8 @@ export default class About extends Component {
                 who understands
                 the highs and lows of being a mum.
             </p>
+          </div>
+        
       </div>
 
     );
