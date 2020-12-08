038f2c05c241e9bc11d2906341b6a337e9c876c0
Isaac <isaaclim95@gmail.com>
2020-12-08 16:31:26 +1000

    fixing carousel and about us

--- src/components/about.component.js
+++ src/components/about.component.js
@@ -2,25 +2,30 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
 import image1 from '../img/image1.jpg';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default class About extends Component {
 
   render() {
     return (
-      <div className="about" id="about">
-          <img src={image1}></img>
-          <div className="aboutText">
+      <Container>
+        <Row>
+          <Col><img src={image1} className="about-img"></img></Col>
+          <Col>
+            <h2 style={{ textAlign: "center" }}>About Us</h2>
             <p>
-                My name is Maureen and I have been running a family day care at my place for 15 years.
-                I have Certificate III in Childcare and Diploma in Children Services, and a current CPR, Asthma and
-                Anaphylactics certificate.
-                I live with my husband Donny. I know how important it is to find an educator you can trust and someone
-                who understands
-                the highs and lows of being a mum.
+              My name is Maureen and I have been running a family day care at my place for 15 years.
+              I have Certificate III in Childcare and Diploma in Children Services, and a current CPR, Asthma and
+              Anaphylactics certificate.
+              I live with my husband Donny. I know how important it is to find an educator you can trust and someone
+              who understands
+              the highs and lows of being a mum.
             </p>
-          </div>
-        
-      </div>
+          </Col>
+        </Row>
+      </Container>
 
     );
   }
--- src/components/home.component.js
+++ src/components/home.component.js
@@ -5,33 +5,29 @@ import Carousel from 'react-bootstrap/Carousel'
 import image1 from '../img/image1.jpg';
 import image2 from '../img/image2.jpg';
 import image3 from '../img/image3.jpg';
-import image4 from '../img/image4.jpg';
-import image5 from '../img/image5.jpg';
-import image6 from '../img/image6.jpg';
 
 export default class Home extends Component {
 
   render() {
     return (
-      // <img src={image1}></img>
       <Carousel className="carousel" fade="true" pause="false" nextIcon="" nextLabel="" prevIcon="" prevLabel="">
         <Carousel.Item className="carousel-item">
           <img className="d-block w-100" src={image1} alt="Kids on a boat"></img>
+          <div class="carousel-caption trickcenter">
+            <div>Welcome to Maureen's Family Day Care</div>
+          </div>
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100" src={image2} alt="Kids on a boat"></img>
+          <div class="carousel-caption trickcenter">
+            <div>Welcome to Maureen's Family Day Care</div>
+          </div>
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100" src={image3} alt="Kids on a boat"></img>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={image4} alt="Kids on a boat"></img>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={image5} alt="Kids on a boat"></img>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={image6} alt="Kids on a boat"></img>
+          <div class="carousel-caption trickcenter">
+            <div>Welcome to Maureen's Family Day Care</div>
+          </div>
         </Carousel.Item>
       </Carousel>
     );
