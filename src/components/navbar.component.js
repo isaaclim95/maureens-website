57c5f3c97610248547f346947b15a04779c663b8
Isaac <isaaclim95@gmail.com>
2020-12-21 20:15:04 +1000

    changed navbar

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -12,9 +12,9 @@ export default class TopNavbar extends Component {
                 <Navbar collapseOnSelect expand="lg" className="top-navbar">
                     <Navbar.Brand href="#home" className="navbar-logo">Maureen's Family Day Care</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="#about" className="nav-link" id="about-nav-link">About</Nav.Link>
+                    <Navbar.Collapse id="responsive-navbar-nav" fixed="bottom">
+                        <Nav className="mr-auto" fixed="bottom">
+                            <Nav.Link href="#about" className="nav-link" id="about-nav-link" fixed="bottom">About</Nav.Link>
                             <Nav.Link href="#activities" className="nav-link" id="activities-nav-link">Activities</Nav.Link>
                             <Nav.Link href="#learning-framework" className="nav-link" id="learning-framework-nav-link">Learning framework</Nav.Link>
                             <Nav.Link href="#routine" className="nav-link" id="routine-nav-link">Routine</Nav.Link>
